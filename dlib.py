4be1f5f5b9dae37bed3a579c547ab314c162ce85
mker-bin <zackky.dac12@gmail.com>
2025-12-08 13:32:56 -0300

    .

--- dlib.py
+++ dlib.py
@@ -1,32 +1,44 @@
+# The code includes C representations for future updates
+
+# struct r_mcr {
+#     char dm1[30];
+#     char dm2[30];
+#     char dm3[30];
+# };
 class macros:
     def __init__(self):
         self.dm1 = ""
         self.dm2 = ""
         self.dm3 = ""
 global r_var
-global _macro
-r_var = ""
-_macro = macros()
+global r_mcr
 
+# resouces:
+r_var = ""       # main var ; C representation: char r_var[50];
+r_mcr = macros() # predefined macros
+
+# .m <dm1, dm2, dm3> <value>
 def macro_definer(macro, val):
-    global _macro
+    global r_mcr
     if macro == "dm1":
-        _macro.dm1 = val
+        r_mcr.dm1 = val
     elif macro == "dm2":
-        _macro.dm2 = val
+        r_mcr.dm2 = val
     elif macro == "dm3":
-        _macro.dm3 = val
+        r_mcr.dm3 = val
 
+# set <dm1, dm2, dm3>
 def inst_set(arg1):
     global r_var
+
+    # checks if the macro name exists in r_mcr
     if arg1 == "dm1":
-        r_var = _macro.dm1
+        r_var = r_mcr.dm1
     elif arg1 == "dm2":
-        r_var = _macro.dm2
+        r_var = r_mcr.dm2
     elif arg1 == "dm3":
-        r_var = _macro.dm3
+        r_var = r_mcr.dm3
     else:
         return f"error: macro '{arg1}' does not exist"
 
-def inst_out():
-    return r_var
+# the "out" instruction is not necessary here
